2c9ec5f010850742a130ee7ae4f1a1e167cf5910
Mikailq1 <77197595+Mikailq1@users.noreply.github.com>
2021-09-01 14:50:44 +0200

    whileLoop (passing) (#1)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,30 @@
 public class Main {
     public static String toRoman(int number) {
-        String result = "";
-        for (int i = 0; i < number; i++) {
-            result += "I";
+        String s = "";
+        while (number >= 37) {
+            s += "XXXVII";
+            number -= 37;
         }
-        return result;
+        while (number >= 10) {
+            s += "X";
+            number -= 10;
+        }
+        while (number >= 9) {
+            s += "IX";
+            number -= 9;
+        }
+        while (number >= 5) {
+            s += "V";
+            number -= 5;
+        }
+        while (number >= 4) {
+            s += "IV";
+            number -= 4;
+        }
+        while (number >= 1) {
+            s += "I";
+            number -= 1;
+        }
+        return s;
     }
 }
